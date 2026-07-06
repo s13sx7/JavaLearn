ce33f068ec0e4d332ccad6e3fc7added58594354
Andrey <s13sx7@gmail.com>
2026-07-06 17:59:31 +0700

    First prog

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+// Это класс Main
+public class Main {
+    //Эта статическая функция является точкой входа в программу
+    static void main(String[] args) {
+        //Это выведение строки на экран, в этом IDE сть сокращение sout
+        System.out.println("My name is Andrey.");
+    }
+}
