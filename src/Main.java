a726bb40c6b2de4d4a43097133a4761a7591764f
Andrey <s13sx7@gmail.com>
2026-07-06 18:08:04 +0700

    new package

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-// Это класс Main
-public class Main {
-    //Эта статическая функция является точкой входа в программу
-    static void main(String[] args) {
-        //Это выведение строки на экран, в этом IDE сть сокращение sout
-        System.out.println("My name is Andrey.");
-    }
-}
---
+++ src/syntax/Main.java
@@ -0,0 +1,10 @@
+package syntax;
+
+// Это класс syntax.Main
+public class Main {
+    //Эта статическая функция является точкой входа в программу
+    static void main() {
+        //Это выведение строки на экран, в этом IDE сть сокращение sout
+        System.out.println("My name is Andrey.");
+    }
+}
